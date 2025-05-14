e61edcca9afe28d29b85364fe628166c80a0dfe5
Fotis Dimitrakopoulos <f.p.dimitrakopoulos@gmail.com>
2025-05-14 23:12:32 +0300

    Modify header in hero banner

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -10,9 +10,14 @@ const HeroBanner = () => {
               <div className="block">
                 {/*<div className="divider mb-3"></div>*/}
                 {/*<span className="text-uppercase text-sm letter-spacing ">Professional Care For Your Health</span>*/}
-                <h1 className="mb-3">Χειρουργός Οφθαλμίατρος Καλαμάτα – Ζήσης Δ. Βάνης</h1>
+                <div className="d-flex flex-column">
+                  <h1 className="mb-3">Χειρουργός Οφθαλμίατρος Καλαμάτα </h1>
+                  <h1 className="mb-3">Ζήσης Δ. Βάνης</h1>
+                </div>
 
-                <p>Το Οφθαλμολογικό Ιατρείο του κου Ζήση Βάνη βρίσκεται στο κέντρο της Καλαμάτας, προσφέροντας υψηλού επιπέδου υπηρεσίες για την υγεία των ματιών σας. Με εξειδικευμένη εκπαίδευση, σύγχρονο εξοπλισμό και φιλική προσέγγιση, παρέχουμε λύσεις που καλύπτουν τις ανάγκες κάθε ασθενούς.</p>
+                <p>Το Οφθαλμολογικό Ιατρείο του κου Ζήση Βάνη βρίσκεται στο κέντρο της Καλαμάτας, προσφέροντας υψηλού
+                  επιπέδου υπηρεσίες για την υγεία των ματιών σας. Με εξειδικευμένη εκπαίδευση, σύγχρονο εξοπλισμό και
+                  φιλική προσέγγιση, παρέχουμε λύσεις που καλύπτουν τις ανάγκες κάθε ασθενούς.</p>
                 <div className="btn-container ">
                   <a href="#appoinment" className="btn btn-main-2 btn-icon btn-round-full">ΚΛΕΙΣΤΕ ΡΑΝΤΕΒΟΥ
                     <i className="icofont icofont-simple-right ml-2  "></i></a>
